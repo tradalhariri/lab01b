e2b439b2b34555e2ccfaf811a97a8792bb08fd00
tradalhariri <trad.alhariri@gmail.com>
2021-06-20 17:02:41 +0300

    create app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let yourName = prompt("What is your name ?");
+alert("Greeting "+yourName);
+let favSport = prompt("What is your favorite sport ?");
+alert("Your favorite sport is  "+favSport);
+let favFood = prompt("What is your favorite Food ?");
+alert("Your favorite Food is  "+favFood);
+let countriesVisited = prompt("What are countries you visited?");
+alert("you visited these countries "+favFood);
+
+alert("Greeting "+yourName+" your favorite sport "+favSport +" and your favorite food "+favFood+" are also like mine and I will visit some countries you visited "+countriesVisited );
+
+
+
+
+document.getElementById("last-p").innerHTML = "Greeting "+yourName+" your favorite sport "+favSport +" and your favorite food "+favFood+" are also like mine and I will visit some countries you visited "+countriesVisited ;
+
+console.log(yourName);
+console.log(favSport);
+console.log(favFood);
+console.log(countriesVisited);
